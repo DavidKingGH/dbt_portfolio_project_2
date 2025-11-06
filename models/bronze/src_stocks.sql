5a8bac25dcd482733e9a82a7e8629792734dd78e
David.King <David-King@live.com>
2025-11-06 14:52:13 -0500

    Cleaned up dbt project file directory discrepancy to enable CI/CD.

--- models/bronze/src_stocks.sql
+++ models/bronze/src_stocks.sql
@@ -7,7 +7,7 @@ select
     trade_date::date as trade_date, 
     ticker::text as ticker, 
     "open"::double as "open", 
-    high:double as high, 
+    high::double as high, 
     low::double as low, 
     "close"::double as "close", 
     volume::double as volume, 
